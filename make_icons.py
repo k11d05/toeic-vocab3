6b5584505a585392cde535617fc2bfc1d8c18ddc
k11d05 <k11dtennsai@gmail.com>
2026-04-19 18:00:54 +0900

    Add files via upload

---
+++ make_icons.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# Run this to generate placeholder icons
+# In practice user can replace with real icons
+print("Icons should be placed as icon-192.png and icon-512.png in /public")
